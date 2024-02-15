2c21e8d99e2dabe5f4cf48c113e4644412c60969
shiro <trung2004ace@gmail.com>
2024-02-15 14:29:17 +0700

    các thông báo
    thông báo về việc hủy chuyến các thứ

---
+++ src/AirlineManager/Caution.java
@@ -0,0 +1,20 @@
+package AirlineManager;
+
+import javax.swing.JOptionPane;
+
+public class Caution {
+
+	public static void main(String[] args) {
+		//JOptionPane.showMessageDialog(null, "Đăng kí thành công", "Thông báo", JOptionPane.PLAIN_MESSAGE);
+		//JOptionPane.showMessageDialog(null, "Đăng kí thành công", "Thông báo", JOptionPane.INFORMATION_MESSAGE);
+		//JOptionPane.showMessageDialog(null, "Đăng kí thành công", "Thông báo", JOptionPane.QUESTION_MESSAGE);
+		//JOptionPane.showMessageDialog(null, "Thông tin đăng kí không đủ", "Thông báo", JOptionPane.WARNING_MESSAGE);
+		//JOptionPane.showMessageDialog(null, "Đăng kí thành công", "Thông báo", JOptionPane.ERROR_MESSAGE);
+		//JOptionPane.showMessageDialog(null, "Tài khoản không tồn tại", "Thông báo", JOptionPane.ERROR_MESSAGE);
+		//JOptionPane.showMessageDialog(null, "Mật khẩu sai", "Thông báo", JOptionPane.ERROR_MESSAGE);
+		 JOptionPane.showConfirmDialog(null, "Đặt chuyến thành công! Bạn muốn tiếp tục hay không?", "Thông báo", JOptionPane.OK_CANCEL_OPTION);
+		
+		
+		}
+	}
+
