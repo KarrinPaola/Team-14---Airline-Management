c7d894e3308b66a7dbcf2a1b6da0fb5bf2e0005e
KarinPaola <tryingbidulee@gmail.com>
2024-02-28 10:36:05 +0700

    MainPage

--- src/AirlineManager/MainPage.java
+++ src/AirlineManager/MainPage.java
@@ -5,6 +5,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class MainPage extends JFrame {
 
@@ -32,11 +39,42 @@ public class MainPage extends JFrame {
 	 */
 	public MainPage() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 845, 485);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel ListButtonPanel = new JPanel();
+		ListButtonPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		ListButtonPanel.setBounds(6, 6, 215, 445);
+		contentPane.add(ListButtonPanel);
+		ListButtonPanel.setLayout(null);
+		
+		JButton BookingButton = new JButton("Đặt chuyến");
+		BookingButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		BookingButton.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 15));
+		BookingButton.setBounds(35, 100, 150, 50);
+		ListButtonPanel.add(BookingButton);
+		
+		JButton ListFlightButton = new JButton("Các chuyến bay");
+		ListFlightButton.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 15));
+		ListFlightButton.setBounds(35, 200, 150, 50);
+		ListButtonPanel.add(ListFlightButton);
+		
+		JButton AccountButton = new JButton("Tài khoản ");
+		AccountButton.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 15));
+		AccountButton.setBounds(35, 300, 150, 50);
+		ListButtonPanel.add(AccountButton);
+		
+		JPanel MainPanel = new JPanel();
+		MainPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		MainPanel.setBounds(230, 7, 610, 445);
+		contentPane.add(MainPanel);
+		MainPanel.setLayout(null);
 	}
-
 }
